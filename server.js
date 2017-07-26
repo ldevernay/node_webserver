29ca15737f0a869fd29fc7af0fe7d370fc85993e
ldevernay <boblehamster@yahoo.fr>
2017-07-26 11:13:48 +0200

    Send JSON

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello express');
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: "Bob",
+    age: 34
+  }); 
 });
 
 app.listen(3000);

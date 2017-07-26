de1914b219c28f5aab8b5451d797d72cf6f54433
ldevernay <boblehamster@yahoo.fr>
2017-07-26 11:45:16 +0200

    Custom home page

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: "Bob",
-    age: 34
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Greetings from Express!'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {

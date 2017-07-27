1477aa73c4b7fa39d5d42858d092d99755510e82
ldevernay <boblehamster@yahoo.fr>
2017-07-27 10:25:43 +0200

    maintenance mode

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: `Sorry, we are closed!`,
+    endDate: '31/08/2017'
+  });
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+  if(err) {
+    console.log('Unable to append to server.log.');
+  }
+});
+  next();
+})
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());

abe0c9cce23cbd6372fc1655b17df8eb8add2273
ldevernay <boblehamster@yahoo.fr>
2017-07-27 12:33:11 +0200

    heroku port

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+// Set port to env variable for Heroku, 3000 for dev
+const port = process.env.PORT || 3000;
+
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -51,4 +54,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server up and listening to ${port}`);
+});
